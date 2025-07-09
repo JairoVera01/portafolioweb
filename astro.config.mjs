c16bf2910f3ad65522480d77e54e7b35bb395819
Frank Jairo Vera <fj.verachamorro@gmail.com>
2025-07-09 15:13:34 -0500

    Refactor code structure for improved readability and maintainability

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,12 @@ import tailwind from "@astrojs/tailwind";
 
 import sitemap from "@astrojs/sitemap";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://jairovera.dev",
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind(), sitemap(), react()],
   i18n: {
     defaultLocale: "es",
     locales: ["es", "en"], // Ahora incluye "es"
